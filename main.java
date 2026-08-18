87848ca53fd861d17ab810e444085b4e28a040fa
rd250004-jpg <rd250004@famt.ac.in>
2026-08-18 16:06:29 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+        System.out.println("revised file");
+        System.out.println("Hari Hari Bol");
+        System.out.println("Hare Krishna");
+    }
+}
